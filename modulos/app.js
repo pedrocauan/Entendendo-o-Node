fb3d304223a9e878d1b9945474edc8d2402b0845
pedrocauan <mainyasuo92@gmail.com>
2022-07-08 03:58:59 -0300

    Modulos

---
+++ modulos/app.js
@@ -0,0 +1,6 @@
+//PEGA O ARQUIVO DA CALCULADORA
+const calculadora = require("./calculadora")
+
+
+console.log(calculadora.soma(10,20))
+console.log(calculadora.nome)
---
+++ modulos/calculadora.js
@@ -0,0 +1,27 @@
+var nome = "Calculadora V1";
+
+function soma(a,b) {
+    return a + b
+}
+
+function mult(a,b) {
+    return a*b
+}
+
+function sub(a,b) {
+    return a - b
+}
+
+
+function div(a,b) {
+    return a / b
+}
+
+//Exporta a função soma para o app.js
+module.exports = {
+    soma,
+    mult,
+    sub,
+    div,
+    nome
+}
